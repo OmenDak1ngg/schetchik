3dad67dda24400ddf99e212fbdd0026db7ee45fe
OmenDak1ngg <ishmatovmarat4@gmail.com>
2025-04-16 10:19:44 +0500

    update

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
+    private static int LeftMouseButoon = 0;
+
     private float _stepDelay = 0.5f;
     private int _startCounter = 0;
     private int _currentCounter;
     private bool _isCounterIncrement;
+    private Coroutine _coroutine;
 
     public event Action Changed;
     public int CurrentCounter => _currentCounter;
@@ -20,28 +23,30 @@ public class Counter : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(LeftMouseButoon))
         {
             if (_isCounterIncrement == false)
             {
                 _isCounterIncrement = true;
-               StartCoroutine(IncreaseCounter());
+               _coroutine = StartCoroutine(IncreaseCounter());
             }
             else
             {
                 _isCounterIncrement = false;
-                StopCoroutine(IncreaseCounter());
+                StopCoroutine(_coroutine);
             }
         }
     }
 
     private IEnumerator IncreaseCounter()
     {
+        var wait = new WaitForSeconds(_stepDelay);
+
         while (_isCounterIncrement)
         {
             _currentCounter++;
             Changed?.Invoke();
-            yield return new WaitForSeconds(_stepDelay);
+            yield return wait;
         }
     }
 }
